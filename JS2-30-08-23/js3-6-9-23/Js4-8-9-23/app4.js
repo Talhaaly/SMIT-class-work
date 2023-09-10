962b36aa9edcd85961302517daf290156fab0025
Talha Ali AKber <talhaaly103@gmail.com>
2023-09-10 22:00:00 +0500

    loop practice

---
+++ JS2-30-08-23/js3-6-9-23/Js4-8-9-23/app4.js
@@ -0,0 +1,174 @@
+
+/*
+var keyGame = prompt("what is your Value ?")
+
+
+switch (Number(keyGame)) {
+    case 1:
+        document.write("Your value is One" + keyGame)
+        break;
+    case 2:
+        document.write("Your value is two" + " " + keyGame)
+        break;
+    case 3:
+        document.write("Your value is three" + " " + keyGame)
+        break;
+    case 4:
+        document.write("Your value is four" + " " + keyGame)
+        break;
+    case 5:
+        document.write("Your value is five" + " " + keyGame)
+        break;
+    case 6:
+        document.write("Your value is six" + " " + keyGame)
+        break;
+
+    default:
+        alert("Your Value is Wrong" + " " + keyGame)
+        break;
+}
+
+*/
+
+
+
+// Conditional Statement 7 obect
+
+// const person1 ={Name:"Talha", Age:23, Status:"Single"}
+// const person2 = {Name:"Ali", Age:25, Status:"Married"}
+
+// if(Age >18 && Status =="Single") {
+
+//     console.log("value is ture")
+
+// } else{
+//     console.log("Wrong")
+// }
+
+// ---------------------------------------------------
+
+// var person1 = {
+//     name: "Talha",
+//     age: "23",
+//     status: "resident"
+// }
+
+
+// var person2 = {
+//     name: "Ali",
+//     age: "25",
+//     status: "resident"
+// }
+
+// function nicEligible(person2){
+//     if (person1.age > 18  && person1.status =="resident") {
+//         alert("yes MR" + person1.name + "You are Eligible")
+//         } else {
+//             alert("sorry MR" + person2.name + "You are not Eligible")
+//         }
+// }
+
+// nicEligible(person1)
+// nicEligible(person2)
+
+
+
+// -------------------------x-----------------x----------
+// while 
+
+
+// var fuel = 100
+
+// while(fuel > 0){
+//     console.log("chlta rhy ga", fuel)
+//     --fuel
+// }
+
+
+// x--------------x------------------x 
+// do while 
+
+// let  fuel = 0
+
+// do {
+//     console.log("main jaounga")
+// } while (fuel > 0);
+
+
+// x------------------------x--------------------x 
+// for loop 
+
+// initilization       condition           inc/dec 
+
+// for(let num = 0; num < 10; num++){
+//     console.log(num)
+// }
+// for(let num = 10; num >= 10; num--){
+//     console.log(num)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app5.js
@@ -0,0 +1,55 @@
+
+// for(let i =1; i <=10; i++){
+//     document.write("2 X" + i + "=" + (i*2) + "<br>")
+// }
+
+
+// x------------------------------------x-------------------x 
+
+// var table = +prompt("Which Table do you want")
+// if(!isNaN(table)){
+// for(let i =1; i <=10; i++){
+//     document.write(table + "X" + i + "=" + (i*table) + "<br>")
+// }
+// }else{
+// alert( table+ "Is Not A Number")
+// }
+
+// x--------------------x-----------------------------------------x--- 
+
+
+// document.write("2 X 1 = 2 <br>")
+// document.write("2 X 2 = 4 <br>")
+// document.write("2 X 3 = 6 <br>")
+// document.write("2 X 4 = 8 <br>")
+// document.write("2 X 5 = 10 <br>")
+// document.write("2 X 6 = 12 <br>")
+// document.write("2 X 7 = 14 <br>")
+// document.write("2 X 8 = 16 <br>")
+// document.write("2 X 9 = 18 <br>")
+// document.write("2 X 10 = 20 <br>")
+
+// x----------------------x-----------------x-----------x 
+
+
+
+
+// const resturantWalay = ["biryani","dalchawal","korma","burger","tikka","gola kabab"]
+
+// const myDemand = prompt("Kia Kahana Hay ?")
+
+// var myCounter = false
+
+// for(let i = 0; i < resturantWalay.length; i++){
+//     if(resturantWalay[i] == myDemand) {
+//         myCounter = i
+//     }
+// }
+// //0
+// // true
+
+// if(!myCounter && typeof myCounter == "boolean"){
+//     alert("Sorry your food is not available")
+// }else {
+//     alert("Han jee Kahna Available Hay counter no" + myCounter + " say mil jaye ga")
+// }
