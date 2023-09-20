94f745b53439af7f05ea7fde557673138d3761d1
Talha Ali AKber <talhaaly103@gmail.com>
2023-09-20 22:03:28 +0500

    class practice

--- js-20-09-23/app20-9.js
+++ js-20-09-23/app20-9.js
@@ -10,20 +10,62 @@
 
 // -----Next Conditions ------ 
 
-let age = prompt("what is your Age ?");
-let gift = prompt("gift lia hay apne?");
-let cake = prompt("cake khaoge ? type yes Agr laye ho ap.");
+// let age = prompt("what is your Age ?");
+// let gift = prompt("gift lia hay apne?");
+// let cake = prompt("cake khaoge ? type yes Agr laye ho ap.");
 
-if (age > 18 && gift == "yes" || cake == "no"){
-    alert("you are Adult.")
-    let whatGift = prompt ("kiya gift laye ho ?");
-    if(gift == "cycle" || whatGift == "watch" || whatGift == "toys"){
-        alert("wow");
-    }
-    else {
-        alert("wapis jao or gift lay kr ana")
-    }
+// if (age > 18 && gift == "yes" || cake == "no"){
+//     alert("you are Adult.")
+//     let whatGift = prompt ("kiya gift laye ho ?");
+//     if(gift == "cycle" || whatGift == "watch" || whatGift == "toys"){
+//         alert("wow");
+//     }
+//     else {
+//         alert("wapis jao or gift lay kr ana")
+//     }
+// }
+// else {
+//     alert("you are not Adult.")
+// }
+
+
+
+
+
+// class practice.
+
+// variable lookup 
+
+// const globalNmber = 5;
+
+// function add(num1,num2) {
+//     const result = (num1 + num2) + globalNmber;
+//     return result;
+    
+// }
+
+// console.log(add(5,3));
+
+
+// callBack functions, higher order functions , functions as first class objet/citizens.
+
+function morning() {
+    console.log('good morning Bob');
+    return 'good morning Bob';
+    
 }
-else {
-    alert("you are not Adult.")
+
+function greet(name, cb) {
+    cb();
+    const myName = "jhon";
+    console.log(` ${name}, my name is ${myName}`);
+    
 }
+
+
+greet('bobo', morning);
+greet('peter', morning);
+
+
+
+
