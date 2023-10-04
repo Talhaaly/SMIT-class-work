b04481347261abbce4f9e3b9d7f670a8b723b2c7
Talha Ali AKber <talhaaly103@gmail.com>
2023-10-04 21:12:10 +0500

    date

--- 04-10-2023/4-10.js
+++ 04-10-2023/4-10.js
@@ -21,11 +21,51 @@
 
 
 
-const number = 89;
+// const number = 89;
 
-const result = Math.sqrt(number);
+// const result = Math.sqrt(number);
 
-console.log(result);
+// console.log(result);
+
+
+// const final = Math.floor(result)
+// console.log(final)
+
+
+
+// const minimum = (1,2,3,4,5,6)
+
+// const minResult = Math.min(minimum)
+// console.log(minResult)
+
+
+
+// Date 
+
+
+// const dateToDay = new Date()
+
+// This is because the getMonth method returns a zero-based number between 0 and 11 . 
+// For example, January is 0 , February is 1 , March is 2 , etc.
+//  If you need to get a one-based value for the month, simply add 1 to the result.
+
+// console.log(dateToDay.getMonth());
+
+
+
+
+
+
+
+// const dob = new Date ('4/5/1999');
+
+// console.log(dob)
+
+
+
+
+
+
 
 
 
