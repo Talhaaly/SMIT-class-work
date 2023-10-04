d28fe2da2fb9f4cabfa9c61cef9a868388db11ef
Talha Ali AKber <talhaaly103@gmail.com>
2023-10-04 20:43:06 +0500

    math function

---
+++ 04-10-2023/4-10.js
@@ -0,0 +1,63 @@
+//  Math Function 
+
+// const num = 4.6678;
+// const result = Math.floor(num);
+
+// console.log(result)
+
+
+// const num1 = 6.7678;
+// const results = Math.ceil(num1);
+
+// console.log(results)
+
+
+// const num2 = 6.4678;
+
+
+// const result3 = Math.round(num2);
+
+// console.log(result3)
+
+
+
+const number = 89;
+
+const result = Math.sqrt(number);
+
+console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
