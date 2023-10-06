33a781b470d47589b372135914f6988e2068c51e
Talha Ali AKber <talhaaly103@gmail.com>
2023-10-06 19:38:10 +0500

    array

---
+++ 06-10-23/6-10.js
@@ -0,0 +1,10 @@
+// const numbers = [220, 550, 320, 560, 660]
+
+// const strings = ["Apple", "Mango", "Banana", "Orange", "Pinaple"]
+
+// for(i=0; i < numbers.length; i++) {
+//     console.log(numbers[i])
+//     for(j=0; j < strings.length; j++ ){
+//         console.log(numbers[i] + strings[j])
+//     }
+// }
