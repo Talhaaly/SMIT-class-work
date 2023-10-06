7e7fc52af769c20a21903bed274fbc0638625424
Talha Ali AKber <talhaaly103@gmail.com>
2023-10-06 20:49:40 +0500

    chapters

--- 06-10-23/6-10.js
+++ 06-10-23/6-10.js
@@ -8,3 +8,115 @@
 //         console.log(numbers[i] + strings[j])
 //     }
 // }
+
+
+
+
+
+
+
+
+
+// var name = "Talha";
+
+// console.log(name.charAt(2));
+
+
+
+
+
+
+
+
+
+
+// var text = " i am talha";
+
+// // console.log(text.indexOf("talha"));
+// var indexOfTalha = text.indexOf("talha")
+
+// if(text !== -1){
+//     console.log(`${text.slice(0, text.indexOf('talha'))} ali`)
+// }
+
+
+// // 0r Replace method 
+// var abc = " ALI"
+// var newText = text.replace("talha", abc);
+
+// console.log(newText)
+
+
+
+
+
+
+
+
+
+
+
+
+// parseFloat 
+// parseInt 
+
+// var num = "1.98";
+
+// console.log(parseInt(num))
+// console.log(parseFloat(num))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// num to str and str to nu convert 
+
+
+// var strNum = "23";
+
+
+// console.log(Number(strNum));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
