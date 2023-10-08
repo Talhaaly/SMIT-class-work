571d811826eacef0d8b48d9ad363b4f6e1f39987
Talha Ali AKber <talhaaly103@gmail.com>
2023-10-08 21:24:13 +0500

    testing

--- 08-10-23/8-10.js
+++ 08-10-23/8-10.js
@@ -3,31 +3,55 @@
 // document.body.style.color/something...
 //Use - .getElementById
 
-// function darkHandler (){
+
+
+
+const bulb = document.getElementById('bulb')
+
+console.log(bulb, '-=--> bulb')
+
+function darkHandler (){
 // document.body.style.background = 'Black'
 // document.body.style.color = 'white'
-// darkBtn.style.display = 'none'
-// lightBtn.style.display = 'block'
-// }
-// function lightHandler (){
+darkBtn.style.display = 'none'
+lightBtn.style.display = 'block'
+bulb.src = "./bulb1.jpg";
+}
+
+function lightHandler (){
 // document.body.style.background = 'green'
 // document.body.style.color = 'white'
-// darkBtn.style.display = 'block'
-// lightBtn.style.display = 'none'
-// }
+darkBtn.style.display = 'block'
+lightBtn.style.display = 'none'
+bulb.src = "./bulb2.jpg";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // turn ON function 
 
 
-function bulbon(){
-    let bulbobj = document.querySelector(".bulboff")
-    bulbobj.setAttribute("src", "./bulb1.jpg");
-}
-function bulboff(){
-    let bulbobj = document.querySelector(".bulboff")
-    bulbobj.setAttribute("src", "./bulb2.jpg");
-}
+// function bulbon(){
+//     let bulbobj = document.querySelector(".bulboff")
+//     bulbobj.setAttribute("src", "./bulb1.jpg");
+// }
+// function bulboff(){
+//     let bulbobj = document.querySelector(".bulboff")
+//     bulbobj.setAttribute("src", "./bulb2.jpg");
+// }
 
 
 
