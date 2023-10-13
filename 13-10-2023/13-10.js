8b6fe1307465cd802885dc27f937004c94192303
Talha Ali AKber <talhaaly103@gmail.com>
2023-10-13 19:22:09 +0500

    quiz check

---
+++ 13-10-2023/13-10.js
@@ -0,0 +1,27 @@
+// Quiz Check 
+
+// const numbers = [1, 2, 8]; 
+// numbers.sort();
+// console.log(numbers[1])
+
+
+
+// const number = [ 10 , 5, 2]; 
+// number.sort();
+// console.log(number[1])
+
+
+// const isTrue = true == [];
+// const isFalse = true == ![];
+
+// console.log(isTrue + isFalse);
+
+
+// console.log(('b' + 'a' + + 'a' + 'a').toLowerCase());
+
+// console.log(018 - 015);
+
+// console.log(typeof NaN);
+
+// X-----------------------------------------------------------X-----------------------------------------------X
+
