3d13d3dd23ffa961ec5ce0fdca5a52302b6f07d2
Talha Ali AKber <talhaaly103@gmail.com>
2023-10-13 21:58:10 +0500

    Dom Bck

--- 13-10-2023/13-10.js
+++ 13-10-2023/13-10.js
@@ -25,3 +25,41 @@
 
 // X-----------------------------------------------------------X-----------------------------------------------X
 
+
+
+// child node vs childernNode-->(White Space D/f)
+// node object per forEache loop lagy ga. 
+// Html collection per forEach loop nh chly ga 
+// getAttribute and setAttribute 
+// display none jaga bhi hatata hay 
+// display hidden jaga mh hatata 
+
+
+
+
+// Task 
+
+// ===========================================================
+
+// wrong
+
+// let array = ["Red", "Green", "Blue", "Grey", "Black"];
+// function press() {
+//     if(array == "array"){
+//         const value = document.getElementById("clickMe")
+//         clickMe.Math.random() * 5;
+//     }
+// }
+
+// =================================================================
+
+
+
+const colors = ["Red", "Green", "Blue", "Grey", "Black", "yellow", "pink"];
+var text = document.querySelector('span')
+const random = Math.floor(Math.random() * 7);
+function clrHndlr() {
+console.log(colors[random])
+text.textContent = colors[random]
+document.body.style.backgroundColor = colors[random]
+}
